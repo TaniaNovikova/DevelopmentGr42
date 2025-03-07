85b859934b62313ecd2cbe92e2c1a9d2024c3db3
TaniaNovikova <tania.novikov@web.de>
2025-03-07 17:07:43 +0100

    RestApiMovieControllerIT

--- src/test/java/de/ait/javalessons/controller/RestApiMovieControllerIT.java
+++ src/test/java/de/ait/javalessons/controller/RestApiMovieControllerIT.java
@@ -21,21 +21,65 @@ public class RestApiMovieControllerIT {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    private RestApiMovieController restApiMovieController;
+
+    @BeforeEach
+    void setUp() {
+        restApiMovieController = new RestApiMovieController();
+    }
+
     @Test
     public void testGetAllMovies() {
         ResponseEntity<Movie[]> response = restTemplate.getForEntity("/movies", Movie[].class);
         assertEquals(200, response.getStatusCodeValue());
-        assertNotNull(response.getBody());
+        assertNotNull(response.getBody().length);
+        assertEquals(5, response.getBody().length);
     }
 
     @Test
-    public void testAddMovie() {
-        Movie movie = new Movie(5L, "Titanic", "Romance", 1997);
+    public void testGetMovieById() {
+        ResponseEntity<Movie> response = restTemplate.getForEntity("/movies/3", Movie.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(3L, response.getBody().getId());
+        assertEquals("The Shape of Water", response.getBody().getTitle());
+    }
+
+    @Test
+    public void testGetMovieByInvalidId() {
+        ResponseEntity<Movie> response = restTemplate.getForEntity("/movies/10", Movie.class);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+
+
+    @Test
+    public void testAddMovieWithCorrectId() {
+        Movie movie = new Movie(6L, "Matrix", "Sci-fi", 1999);
         ResponseEntity<Movie> response = restTemplate.postForEntity("/movies", movie, Movie.class);
         assertEquals(201, response.getStatusCodeValue());
+        assertNotNull(response.getBody());
+        assertEquals(6L, response.getBody().getId());
+    }
 
-        ResponseEntity<Movie[]> updatedList = restTemplate.getForEntity("/movies", Movie[].class);
-        assertTrue(updatedList.getBody().length > 3);
+    @Test
+    public void testAddMovieWithExistingId() {
+        Movie newMovie = new Movie(1L, "Interstellar", "Sci-Fi", 2014);
+        ResponseEntity<Movie> response = restTemplate.postForEntity("/movies", newMovie, Movie.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(6L, response.getBody().getId());
+    }
+
+    @Test
+    public void testAddMovieWithIncorrectId() {
+        Movie newMovie = new Movie(10L, "The Matrix", "Action", 1999);
+        ResponseEntity<Movie> response = restTemplate.postForEntity("/movies", newMovie, Movie.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(6L, response.getBody().getId());
     }
 
     @Test
